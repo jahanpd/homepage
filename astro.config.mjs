cb3e33d2773a9c109013130f06373cc953b3f515
jahan <jahan.pd@gmail.com>
2023-10-03 12:51:34 +1100

    not ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
@@ -11,6 +9,4 @@ export default defineConfig({
     // Example: Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
   },
-  output: "server",
-  adapter: vercel()
 });
